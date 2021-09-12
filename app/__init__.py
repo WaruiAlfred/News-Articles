461925ddf7bb74aad608f50a2ab93747b3392001
Alfred Warui <alfredwarui17@gmail.com>
2021-09-13 01:05:35 +0300

    create app initialization

---
+++ app/__init__.py
@@ -0,0 +1,26 @@
+from flask import Flask 
+from config import config_options
+from flask_bootstrap import Bootstrap
+
+bootstrap = Bootstrap()
+
+def create_app(config_name):
+  
+  # Initializing the application
+  app = Flask(__name__,instance_relative_config = True)
+
+  # Setting up app configuration
+  app.config.from_object(config_options[config_name])
+
+  # Initializing Flask Bootstrap Extension
+  bootstrap.init_app(app)
+
+  # Registering the blueprint
+  from .main import main as main_blueprint
+  app.register_blueprint(main_blueprint)
+  
+  # Setting app configuration
+  from .api_requests import configure_api_requests
+  configure_api_requests(app)
+  
+  return app
